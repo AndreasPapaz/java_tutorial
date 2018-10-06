bd683495ddfa340aa8c253bac58132f646625052
AndreasPapaz <andreaspapaz@gmail.com>
2018-10-06 12:52:25 -0500

    monster games

--- src/HelloMonster.java
+++ src/HelloMonster.java
@@ -1,10 +1,21 @@
+import java.util.Arrays;
+//import org.apache.commons.lang3.ArrayUtils;
+
 public class HelloMonster {
 
     public static void main(String[] args) {
+        Monster.buildBattleBoard();
 
-        Monster Frank = new Monster();
+        char[][] tempBattleBoard = new char[10][10];
 
-        System.out.print(Frank.TOMBSTONE);
+        //ObjectName[] ArrayName = new ObjectName[4];
+        Monster[] Monsters = new Monster[4];
+        Monsters[0] = new Monster(1000, 20, 1, "Frank");
+        Monsters[1] = new Monster(1000, 20, 1, "Paul");
+        Monsters[2] = new Monster(1000, 20, 1, "George");
+        Monsters[3] = new Monster(1000, 20, 1, "Darko");
+
+        Monster.redrawBoard();
     }
 
 }
--- src/Monster.java
+++ src/Monster.java
@@ -11,17 +11,42 @@ public class Monster {
         }
     }
 
+    public static void redrawBoard() {
+        int k = 1;
+        while(k <= 30){
+            System.out.print('-');
+            k++;
+        }
+        System.out.println();
+
+        for(int i = 0; i < battleBoard.length; i++) {
+            for(int j = 0; j < battleBoard[i].length; j++) {
+                System.out.print("|" + battleBoard[i][j] + "|");
+            }
+            System.out.println();
+        }
+
+        int p = 1;
+        while(p <= 30){
+            System.out.print('-');
+            p++;
+        }
+
+    }
+
 
     public final String TOMBSTONE = "Here Lies a Dead monster";
 
     private int health = 500;
     private int attack = 20;
     private int movement = 2;
-    private int xPos = 0;
-    private int yPos = 0;
     private boolean alive = true;
 
-    public String name = "Big Mongster";
+    public String name = "Big Monster";
+    public char nameChar1 = 'B';
+    public int xPos = 0;
+    public int yPos = 0;
+    public static int numOfMonsters = 0;
 
 
     public int getAttack() {
@@ -34,6 +59,10 @@ public class Monster {
 
     public int getHealth() {
         return health;
+    }
+
+    public boolean getAlive() {
+        return alive;
     }
 
     public void setHealth(int decHealth) {
@@ -53,16 +82,35 @@ public class Monster {
         }
     }
 
-    public Monster(int health, int newAttack, int newMovement) {
+    public Monster(int health, int attack, int movement, String name) {
         this.health = health;
-        attack = newAttack;
-        movement = newMovement;
+        this.attack = attack;
+        this.movement = movement;
+        this.name = name;
 
+        int maxXBoardSpace = battleBoard.length - 1;
+        int maxYBoardSpace = battleBoard[0].length - 1;
+
+        int randNumX, randNumY;
+
+        do {
+
+            randNumX = (int) (Math.random() * maxXBoardSpace);
+            randNumY = (int) (Math.random() * maxYBoardSpace);
+
+        } while(battleBoard[randNumX][randNumY] != '*');
+
+        this.xPos = randNumX;
+        this.yPos = randNumY;
+
+        this.nameChar1 = this.name.charAt(0);
+
+        battleBoard[this.yPos][this.xPos] = this.nameChar1;
     }
 
     // Default Constructor
     public Monster() {
-
+        numOfMonsters++;
     }
 
 }
